88ae9086dcddc76be09c6370c5d5c0d6719c16fb
Pete T <pete@blueseabluesky.com>
2026-01-07 08:34:53 +0000

    tweaks to day 0 stuff...

--- scripts/AoC_2025/day0.py
+++ scripts/AoC_2025/day0.py
@@ -1,8 +1,13 @@
+VERBOSE = False
+
 #Need this in each day
 def run(part:int, input_data:list):
   print(f"Day 0 - Part {part}")
   print(f"---First line length = {len(input_data[0])}---")
   print(f"---Number of rows = {len(input_data)}---")
+  
+  if VERBOSE : print(f"Input Data: {input_data}")
+
   ans = 0
   if part == 1 :
     ans = 0
